2d3b9b375e22b25501c5c81a44219d9833641dda
Guinn <eguinjonathan08@gmail.com>
2024-09-21 09:42:26 +0700

    created website version

--- index.js
+++ index.js
@@ -1,40 +1,41 @@
-function generatePassword(
-  allowUppercase,
-  allowLowercase,
-  allowNumbers,
-  allowsymbols,
-  length
-) {
+const uppercasechecked = document.getElementById("uppercase");
+const lowercasechecked = document.getElementById("lowercase");
+const numberschecked = document.getElementById("numbers");
+const symbolschecked = document.getElementById("symbols");
+const button = document.getElementById("generate");
+const passwordshow = document.getElementById("password");
+const passwordlength = document.getElementById("lengthpassword").value;
+
+button.onclick = function () {
+  generatePassword();
+};
+
+function generatePassword() {
   let password = "";
   const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const lowercase = "abcdefghijklmnopqrstuvwxyz";
   const numbers = "0123456789";
   const symbols = "!@#$%^&*()_+~`|}{[]:;?><,./-=\\";
+  length = passwordlength;
 
   let allowedcharacters = "";
 
-  allowedcharacters += allowUppercase ? uppercase : "";
-  allowedcharacters += allowLowercase ? lowercase : "";
-  allowedcharacters += allowNumbers ? numbers : "";
-  allowedcharacters += allowsymbols ? symbols : "";
+  allowedcharacters += uppercasechecked.checked ? uppercase : "";
+  allowedcharacters += lowercasechecked.checked ? lowercase : "";
+  allowedcharacters += numberschecked.checked ? numbers : "";
+  allowedcharacters += symbolschecked.checked ? symbols : "";
 
-  for (let i = 0; i < length; i++) {
+  if (allowedcharacters === "") {
+    passwordshow.textContent = "Please select atleast one option";
+    return;
+  } else if (passwordlength.value < 1 || passwordlength.value > 128) {
+    passwordshow.textContent = "Please enter a number between 1 and 128";
+    return;
+  }
+
+  for (let i = 0; i < passwordlength; i++) {
     let index = Math.floor(Math.random() * allowedcharacters.length);
     password += allowedcharacters[index];
   }
-  console.log(password);
+  passwordshow.textContent = password;
 }
-
-const allowUppercase = true;
-const allowLowercase = true;
-const allowNumbers = true;
-const allowsymbols = true;
-const length = 12;
-
-generatePassword(
-  allowUppercase,
-  allowLowercase,
-  allowNumbers,
-  allowsymbols,
-  length
-);
